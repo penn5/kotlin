d827334b58a6805e4e51d19dad99b2ccf2ce50d5
Vladimir Dolzhenko <vladimir.dolzhenko@jetbrains.com>
2020-03-13 14:41:38 +0100

    Fix ExtensionTestUtil compilation for 191; CustomUsageSearcherTest compilation for 201
    Relates to #KT-36657

--- idea/tests/org/jetbrains/kotlin/findUsages/CustomUsageSearcherTest.kt
+++ idea/tests/org/jetbrains/kotlin/findUsages/CustomUsageSearcherTest.kt
@@ -5,21 +5,19 @@
 
 package org.jetbrains.kotlin.findUsages
 
+import com.intellij.find.findUsages.CustomUsageSearcher
+import org.jetbrains.kotlin.idea.KotlinFileType
+import org.jetbrains.kotlin.idea.maskExtensions
 import org.jetbrains.kotlin.idea.test.KotlinLightCodeInsightFixtureTestCaseBase
 
 class CustomUsageSearcherTest : KotlinLightCodeInsightFixtureTestCaseBase() {
 
     fun testAddCustomUsagesForKotlin() {
-//        TODO("[VD] There are diff API for ExtensionTestUtil in 191, 192, 193")
-//        val customUsageSearcher = object : CustomUsageSearcher() {
-//            override fun processElementUsages(element: PsiElement, processor: Processor<Usage>, options: FindUsagesOptions) {
-//                runReadAction { processor.process(UsageInfo2UsageAdapter(UsageInfo(element))) }
-//            }
-//        }
-//        // ExtensionTestUtil.maskExtensions(CustomUsageSearcher.EP_NAME, listOf(customUsageSearcher), testRootDisposable)
-//        myFixture.configureByText(KotlinFileType.INSTANCE, """val <caret>selfUsed = 1""")
-//
-//        val usages = myFixture.getUsageViewTreeTextRepresentation(myFixture.elementAtCaret)
-//        assertTrue(usages.contains("val selfUsed"))
+        val customUsageSearcher = MyCustomUsageSearcher()
+        maskExtensions(CustomUsageSearcher.EP_NAME, listOf(customUsageSearcher), testRootDisposable)
+        myFixture.configureByText(KotlinFileType.INSTANCE, """val <caret>selfUsed = 1""")
+
+        val usages = myFixture.getUsageViewTreeTextRepresentation(myFixture.elementAtCaret)
+        assertTrue(usages.contains("val selfUsed"))
     }
 }
